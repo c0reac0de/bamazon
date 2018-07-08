48aa83a6a2223866070a3f38749e8eb213fd1633
c0reac0de <candice.correa@gmail.com>
2018-07-08 19:37:49 -0400

    sql connection timeout

--- bamazon.sql
+++ bamazon.sql
@@ -6,14 +6,27 @@ create database bamazon_db;
 use bamazon_db; 
 -- create table object--
 create table products(
-					item_id integer (13) auto_increment PRIMARY KEY, -- unique identifier
+					ID INTEGER not null, -- unique identifier
 					product_name varchar (45) not null, -- product name as a string not null
 					department_name varchar (30) not null, -- department name as a string 
-                    price integer/decimal (500) not null, -- price to customer
+                    price integer (500) not null, -- price to customer
 					stock_quantity integer (500), -- how much product available 
+					PRIMARY KEY (ID)
 );
 
 
 -- INSERT INTO: 10 PRODUCTS --
-
+insert into products 
+(product_name,department_name,price,stock_quantity)
+values 
+(jellybeans,snacks,4,50),
+(magazines,books,6,10),
+(hats,accessories,15,50),
+(earphones,electronics,20,30),
+(chocolate,snacks,5,20),
+(pop,snacks,3,30),
+(sunscreen,seasonal,15,30),
+(summerhats,seasonal,15,10),
+(cameras,electronics,60,30),
+(pulpfiction,books,10,20);
 
--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -6,27 +6,41 @@ var inquirer = require ("inquirer");
 
 //BASIC TASKS
 //display all products to user
-select * from products (in bamazon);
+
+var con= mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "bamazon_db"
+});
+
+con.connect(function(err){
+    if(err) throw err;
+    con.query('select * from products'), function (err, result, fields){
+        if (err) throw err;
+        console.log(result);
+    };
+});
 
 
 //using inquirer module/ prompt
 //create function to ask 
-function chooseProduct(argument/parameters needed){
-    inquirer.prompt([
-        //user which product they want
-        {
-            "product_id":"product_id",
-            "type":"input",
-            "message": "what product would you like?"
-        },
-        //quantity of product needed
-        {
-            "stock_quantity":"stock_quantity",
-            "type":"input",
-            "message": "how many units of this product would you like?"
-        }
-    ]).then(answers)
-}
+// function chooseProduct(argument/parameters needed){
+//     inquirer.prompt([
+//         //user which product they want
+//         {
+//             "product_id":"product_id",
+//             "type":"input",
+//             "message": "what product would you like?"
+//         },
+//         //quantity of product needed
+//         {
+//             "stock_quantity":"stock_quantity",
+//             "type":"input",
+//             "message": "how many units of this product would you like?"
+//         }
+//     ]).then(answers)
+// }
 
 
 //confirm order placed
