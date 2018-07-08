7e3988e521cca1e7b43a17937cfd3aebbba83369
c0reac0de <candice.correa@gmail.com>
2018-07-08 19:47:54 -0400

    mockitems

--- bamazon.sql
+++ bamazon.sql
@@ -6,12 +6,13 @@ create database bamazon_db;
 use bamazon_db; 
 -- create table object--
 create table products(
-					ID INTEGER not null, -- unique identifier
+					item_id integer (13) auto_increment, -- unique identifier
 					product_name varchar (45) not null, -- product name as a string not null
 					department_name varchar (30) not null, -- department name as a string 
-                    price integer (500) not null, -- price to customer
-					stock_quantity integer (500), -- how much product available 
-					PRIMARY KEY (ID)
+                    price integer (200) not null, -- price to customer
+					stock_quantity integer (200), -- how much product available 
+                    primary key (item_id)
+                    
 );
 
 
@@ -19,14 +20,18 @@ create table products(
 insert into products 
 (product_name,department_name,price,stock_quantity)
 values 
-(jellybeans,snacks,4,50),
-(magazines,books,6,10),
-(hats,accessories,15,50),
-(earphones,electronics,20,30),
-(chocolate,snacks,5,20),
-(pop,snacks,3,30),
-(sunscreen,seasonal,15,30),
-(summerhats,seasonal,15,10),
-(cameras,electronics,60,30),
-(pulpfiction,books,10,20);
+('magazines','books',6,10),
 
+('pulpfiction','books',10,20);
+
+insert into products 
+(product_name,department_name,price,stock_quantity)
+values 
+('chewinggum','snacks',2,30), 
+('hats','accessories',15,50), 
+('headbuds','electronics',20,30),
+ ('chocolate','snacks',5,20),
+ ('pop','snacks',3,30),
+ ('sunscreen','seasonal',15,30),
+ ('summerhats','seasonal',15,10),
+ ('cameras','electronics',60,30);
